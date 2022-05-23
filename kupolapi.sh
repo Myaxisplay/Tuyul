fc9bb32ae004b5aaa7d2500cb3e4431fd0d9052d
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-23 23:31:12 +0700

    Update kupolapi.sh

--- kupolapi.sh
+++ kupolapi.sh
@@ -3,15 +3,15 @@ ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
+sleep 10m
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.66.138.250:5896
-socks5_username = ingfoingfo
-socks5_password = maszZeehh
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
