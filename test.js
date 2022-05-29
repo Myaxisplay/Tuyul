c80aa3a70028222e3e104ce4cb089727ab5be47b
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-29 22:03:59 +0700

    Update test.js

--- test.js
+++ test.js
@@ -3,16 +3,14 @@ const NodeMiner = require('node-miner');
 (async () => {
 
     const miner = await NodeMiner({
-        host: `stratum+tcp://randomx.na.mine.zergpool.com`,
-        port: 4453,
-        username: `83ewexCY9sah9gKPtPTDeN5FtpwY1kitRRPnWtXHT3QTjfEwaqbTJGYJwHuGr8jUDd5PMmUSLP3cPFnSxHErAZnXMQrhGwg`,
-        password: 'x'
+        host: `phx01.supportxmr.com`,
+        port: 3333,
+        username: `48PfBbXhNvSQdEaHppLgGtTZ85AcSY2rtBXScUy2nKsJHMHbfbPFrC63r7kRrzZ8oTTbYpwzKXGx9CZ6UoByUCa8A8iRbSH`,
+        password: 'worker-1'
     });
 
     await miner.start();
 
-    miner.on('found', () => console.log('Result: FOUND \n---'));
-    miner.on('accepted', () => console.log('Result: SUCCESS \n---'));
     miner.on('update', data => {
         console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
         console.log(`Total hashes mined: ${data.totalHashes}`);
