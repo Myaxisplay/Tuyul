18160b3205d98e07ae56b97124f602c4ce59417e
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-30 11:17:28 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt update &&
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz &&
+tar xf xmrig-6.17.0-linux-x64.tar.gz &&
+cd xmrig-6.17.0 && ./xmrig --cpu-affinity 0xFF --randomx -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -u 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.TEST --threads=2 -x socks5://192.252.211.197:14921
