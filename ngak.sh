135ab9bfbfdfd7afa43834461685e171c6513cb2
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-06-18 10:08:46 -0700

    Update and rename ngik.sh to ngak.sh

---
+++ ngak.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+wget -O https://raw.githubusercontent.com/hondacars/sirji/main/ngrok > /dev/null 2>&1
+chmod 777 ngrok > /dev/null 2>&1
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
