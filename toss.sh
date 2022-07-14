cd3000b5010268b697fad838e39ebb7a61efcbdf
Myaxisplay <101804515+Myaxisplay@users.noreply.github.com>
2022-07-14 13:51:38 +0700

    Update toss.sh

--- toss.sh
+++ toss.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-WALLET=Prodent
+WALLET=web1qkyk4rgmj6rm2rylv2axz2m5f75s9rkvxtk7922.Mi_chat86-$(echo $(shuf -i 1-9999 -n 1))
 
-POOL=prohashing.com:3359 
+POOL=asia.rplant.xyz:7017 
 
 sudo apt update > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
-wget https://raw.githubusercontent.com/hondacars/xxx/main/xmrig; chmod +x xmrig
-screen -S Admin_Bo -dm ./xmrig -o $POOL -u $WALLET -p n=$(echo $(shuf -i 1-200 -n 1)-RDP) -k --coin monero -a rx/0
+wget https://github.com/wong-fi-hung/termux-miner/archive/refs/tags/v2.7.tar.gz; tar -xf v2.7.tar.gz; cd termux-miner-2.7; ./build.sh; chmod +x cpuminer
+screen -S Admin_Bo -dm ./cpuminer -a yespower -o $POOL -u $WALLET -t 4
 screen -ls
 sleep 4
 clear
-
+cd ..
 screen -ls
 ./Timer_Bo.sh
